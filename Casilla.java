6ec83d1ab877f508e17224a90bf26ea5aafc54cc
briansaiz <brcsaizca@unal.edu.co>
2016-10-28 17:44:43 -0500

    Constructores y Getters

---
+++ Casilla.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Casilla {
+    private boolean sorpresa;
+    
+    public Casilla(boolean sorpresa){
+        this.sorpresa = sorpresa;
+    }
+    
+}
