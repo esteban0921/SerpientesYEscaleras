a7b28fa16c06fa9328e724cbae0948fc9fbba0fa
briansaiz <brcsaizca@unal.edu.co>
2016-10-29 15:45:41 -0500

    colores

---
+++ Dado.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+
+import java.util.Random;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Dado {
+
+    private String color;
+    private int numeroObtenido;
+    public static  String colores[] = {"amarillo", "azul", "rojo","verde"}; 
+
+
+    public Dado(String color) {
+        this.color = color;
+        this.numeroObtenido = 0;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumeroObtenido() {
+        return numeroObtenido;
+    }
+
+    public void lanzarDado() {
+        Random azar = new Random();
+        this.numeroObtenido = (int) (azar.nextDouble() * 6 + 1);
+    }
+}
---
+++ Ficha.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Ficha {
+    private String color;
+    public static  String colores[] = {"amarillo", "azul", "rojo","verde"}; 
+
+
+    public Ficha(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
