57eeb7ea93d50b0cf04d28275baf18b0b64a39a7
Cribaque <crabaqueropi@unal.edu.co>
2016-10-28 18:07:57 -0600

    Agregue método imprimir tablero (funciona)

--- src/presentacion/UI.java
+++ src/presentacion/UI.java
@@ -5,10 +5,55 @@
  */
 package presentacion;
 
+import datos.*;
+
 /**
  *
  * @author LENOVO
  */
 public class UI {
-    
+
+    public static void imprimirTablero(Tablero tablero) {
+        Casilla c[][] = tablero.getCasillas();
+        Escalera e[] = tablero.getEscaleras();
+        boolean hayEscalera = false;
+        Serpiente s[] = tablero.getSerpientes();
+        boolean haySerpiente = false;
+
+        for (int i = 0; i < c.length; i++) {
+            for (int j = 0; j < c[1].length; j++) {
+
+                for (int k = 0; k < e.length; k++) {
+                    if ((e[k].getCasillaInicio()[0] == i) && (e[k].getCasillaInicio()[1] == j)) {
+                        hayEscalera = true;
+                    }
+
+                    if ((e[k].getCasillaFinal()[0] == i) && (e[k].getCasillaFinal()[1] == j)) {
+                        hayEscalera = true;
+                    }
+
+                    if ((s[k].getCasillaInicio()[0] == i) && (s[k].getCasillaInicio()[1] == j)) {
+                        haySerpiente = true;
+                    }
+
+                    if ((s[k].getCasillaFinal()[0] == i) && (s[k].getCasillaFinal()[1] == j)) {
+                        haySerpiente = true;
+                    }
+                }
+
+                if (c[i][j].isSorpresa()) {
+                    System.out.print("?\t");
+                } else if (hayEscalera) {
+                    System.out.print("E\t");
+                    hayEscalera = false;
+                } else if (haySerpiente) {
+                    System.out.print("S\t");
+                    haySerpiente=false;
+                } else {
+                    System.out.print("-\t");
+                }
+            }
+            System.out.println("");
+        }
+    }
 }
