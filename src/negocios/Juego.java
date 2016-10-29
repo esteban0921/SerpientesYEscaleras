735a8dded72ff49f0ced3167a8b4510c41b37ca5
Cribaque <crabaqueropi@unal.edu.co>
2016-10-28 18:31:02 -0600

    Cree un tablero y un jugador para probar impresion

--- src/negocios/Juego.java
+++ src/negocios/Juego.java
@@ -6,6 +6,7 @@
 package negocios;
 
 import datos.*;
+import presentacion.UI;
 
 /**
  *
@@ -21,7 +22,45 @@ public class Juego {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int a[] = {2,6};
+        int b[] = {6,8};
+        Serpiente serpiente1 = new Serpiente(a,b);
+        
+        int aa[] = {0,1};
+        int bb[] = {5,0};
+        Serpiente serpiente2 = new Serpiente(aa,bb);
+        
+        Serpiente s1[] = {serpiente1, serpiente2};
+        
+        int c[] = {9,2};
+        int d[] = {4,3};
+        Escalera escalera1 = new Escalera(c,d);
+        
+        int cc[] = {4,9};
+        int dd[] = {1,8};
+        Escalera escalera2 = new Escalera(cc,dd);
+        
+        Escalera e1[] = {escalera1, escalera2};
+        
+        Casilla casillas[][] = new Casilla[10][10];
+        for (int i = 0; i < casillas.length; i++) {
+            for (int j = 0; j < casillas.length; j++) {
+                casillas[i][j] = new Casilla();  
+            }
+        }
+        casillas[5][5].setSorpresa(true);
+        casillas[2][2].setSorpresa(true);
+        casillas[7][7].setSorpresa(true);
+        casillas[6][1].setSorpresa(true);
+        casillas[3][8].setSorpresa(true);
+        
+        Tablero tablero = new Tablero(casillas,e1,s1);
+        
+        Ficha fichaJugador1 = new Ficha("Perro","Roja");        
+        Jugador jugador1 = new Jugador(1, "Cristian", fichaJugador1); 
+        int[] posicionJugador1 = {9,5};
+        jugador1.setPosicion(posicionJugador1);
+        UI.imprimirTablero(tablero, jugador1.getPosicion());
     }
 
 }
